dffd84b1e31e9758f2d53c38f9125f679c938017
JaquelineMedina <jaquelinegpem@gmail.com>
2022-10-20 18:57:04 -0500

    Original

---
+++ src/router/index.js
@@ -0,0 +1,87 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import HomePage from '../views/HomePage.vue'
+import LoginPage from '../views/LoginPage.vue'
+import SignupPage from '../views/SignupPage.vue'
+import CarritoPage from '../views/CarritoPage.vue'
+import InicioClientePage from '../views/InicioClientePage.vue'
+import ContinuarCompraPage from '../views/ContinuarCompraPage.vue'
+import CuentaClientePage from '../views/CuentaClientePage.vue'
+import MenuPage from '../views/MenuPage.vue'
+import AdministrarProductoPage from '../views/AdministrarProductoPage.vue'
+import NosotrosPage from '../views/NosotrosPage.vue'
+import InicioNormalPage from '../views/InicioNormalPage'
+
+
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupPage
+  },
+  {
+    path: '/carrito',
+    name: 'Carrito',
+    component: CarritoPage
+  },
+  {
+    path: '/inicio_cliente',
+    name: 'InicioCliente',
+    component: InicioClientePage
+  },
+  {
+    path: '/continuar_compra',
+    name: 'ContinuarCompra',
+    component: ContinuarCompraPage
+  },
+
+  {
+    path: '/cuenta_cliente',
+    name: 'CuentaCliente',
+    component: CuentaClientePage
+  },
+
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: MenuPage
+  },
+
+  {
+    path: '/administrar_producto',
+    name: 'AdministrarProducto',
+    component: AdministrarProductoPage
+  },
+
+  {
+    path: '/nosotros',
+    name: 'Nosotros',
+    component: NosotrosPage
+  },
+  {
+    path: '/inicio_normal',
+    name: 'InicioNormal',
+    component :InicioNormalPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
