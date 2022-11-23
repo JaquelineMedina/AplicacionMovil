0f3e3306a5c09801398783efcd51b3e02fdbe22a
JaquelineMedina <jaquelinegpem@gmail.com>
2022-11-23 17:25:37 -0600

    Actualizacion 12.1.1 en login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -12,6 +12,8 @@
 
     <ion-content :fullscreen="true">
       <ion-card class="card-container">
+
+      <ion-img src="assets/img/cds.png" alt="The Wisconsin State Capitol building in Madison, WI at night"></ion-img>
         <ion-card-header>
           <ion-card-title class="card-container__title">INICIA SESIÓN</ion-card-title>
         </ion-card-header>
